3a8ea96537f03684cb8ed727b1bc8589e643baef
branden langhals <58713197+Brandenlanghals@users.noreply.github.com>
2020-07-02 23:11:26 -0400

    Words Words Words were changed

--- src/yourdata.js
+++ src/yourdata.js
@@ -4,16 +4,16 @@ export default
         //Change The Website Template
         name :'Branden.',
         headerTagline: [//Line 1 For Header
-                        'Hi 👋 You found me! I am Branden.',
+                        'Hi I am Branden.',
                         //Line 2 For Header
-                        'Full Stack Developer  ',
+                        'A Full Stack Developer  ',
                         //Line 3 For Header
                         'from Cleveland, Ohio'
     ],
         //Contact Email
         contactEmail:'brandenlanghals@gmail.com',
         // Add Your About Text Here
-        abouttext: "I used to be a moderately successful musician, and now I am a moderately talented software developer. Like many others, I needed a career change and decided to follow through with a passion I had years ago.   Back then, obsessing over my Xanga page, or customizing the details of my  MySpace  was never something I thought could lead to a career. My passion for web development, and my drive for getting things done led me to the Case Western Reserve Coding Bootcamp.   Full stack development cannot be mastered in six months, but the real value is in the effort you put forth to confidently study each skill.  In this time I have developed dynamic applications using HTML, CSS, JavaScript, React, SQL, JSON, AJAX, NodeJS, apps using third-party APIs,  got really good at debugging, and all during the new reality of the COVID-19 pandemic.   With continued learning, I look forward to working with passion, and integrity for a company whose values are aligned with my own.   ",
+        abouttext: "I used to be a moderately successful musician, and now I am a moderately talented software developer. Like many others, I needed a career change and decided to follow through with a passion I had years ago.   Back then, obsessing over my Xanga page, or customizing the details of my  MySpace  was never something I thought could lead to a career. My passion for web development, and my drive for getting things done led me to the Case Western Reserve University Coding Bootcamp.  I am aware that full stack development cannot be mastered in six months, but the real value is the effort I put forth to confidently study each skill.  In this time I have developed dynamic applications using HTML, CSS, JavaScript, React, SQL, JSON, AJAX, NodeJS, apps using third-party APIs,  got really good at debugging, and all during the new reality of the COVID-19 pandemic.   With continued learning, I look forward to working with passion, and integrity for a company whose values are aligned with my own.   ",
         aboutImage:'https://i.ibb.co/nkwkH4V/tie5.png',
        //Change This To Hide The Image of About Section (True Or False)
        ShowAboutImage:true,//true or false (Change Here)
