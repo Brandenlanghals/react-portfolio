cd2dc82020316d6653368d190768be75ffbe5419
branden langhals <58713197+Brandenlanghals@users.noreply.github.com>
2020-07-10 21:38:29 -0400

    updating projects

--- src/yourdata.js
+++ src/yourdata.js
@@ -13,7 +13,7 @@ export default
         //Contact Email
         contactEmail:'brandenlanghals@gmail.com',
         // Add Your About Text Here
-        abouttext: "I used to be a moderately successful musician, and now I am a moderately talented software developer. Like many others, I needed a career change and decided to follow through with a passion I had years ago.   Back then, obsessing over my Xanga page, or customizing the details of my  MySpace  was never something I thought could lead to a career. My passion for web development, and my drive for getting things done led me to the Case Western Reserve University Coding Bootcamp.  I am aware that full stack development cannot be mastered in six months, but the real value is the effort I put forth to confidently study each skill.  In this time I have developed dynamic applications using HTML, CSS, JavaScript, React, SQL, JSON, AJAX, NodeJS, apps using third-party APIs,  got really good at debugging, and all during the new reality of the COVID-19 pandemic.   With continued learning, I look forward to working with passion, and integrity for a company whose values are aligned with my own.   ",
+        abouttext: "I used to be a moderately successful musician, and now I am a moderately talented software developer. Like many others, I needed a career change and decided to follow through with a passion I had years ago.   Obsessing over my Xanga page, or customizing the details of my  MySpace  was never something I thought could lead to a career. My passion for web development, and my drive for getting things done led me to the Case Western Reserve University Coding Bootcamp.  I am aware that full stack development cannot be mastered in six months, but the real value is the effort I put forth to confidently study each skill.  In this time I have developed dynamic applications using HTML, CSS, JavaScript, React, SQL, JSON, AJAX, NodeJS, apps using third-party APIs,  got really good at debugging, and all during the new reality of the COVID-19 pandemic.   With continued learning, I look forward to working with passion, and integrity for a company whose values are aligned with my own.   ",
         aboutImage:'https://i.ibb.co/nkwkH4V/tie5.png',
        //Change This To Hide The Image of About Section (True Or False)
        ShowAboutImage:true,//true or false (Change Here)
@@ -21,33 +21,33 @@ export default
        projects:[
            {
             id: 1,//DO NOT CHANGE
-            title:"I'm Hungry", //Project Title 
-             service:'UI/UX Design', // Service Type
+            title:"I'm Hungry App", //Project Title 
+             service:'Project One', // Service Type
             //Project Image 
              imageSrc:"https://images.unsplash.com/photo-1558104631-0fa41a8f6c20?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
              //Project URL
-             url:'http://chetanverma.com/'
+             url:'https://brandenlanghals.github.io/imhungryapp/'
             },
             {
                 id: 2,//DO NOT CHANGE THIS (Please)😅
-                title: 'Project Two',
-                service: 'Website Development',
+                title: 'Everyday Sensei',
+                service: 'Project Two',
                 imageSrc: "https://images.unsplash.com/photo-1554866585-cd94860890b7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
-                url: 'http://chetanverma.com/'
+                url: 'https://github.com/Brandenlanghals/EverydaySensei'
             },
             { 
                 id: 3,//DO NOT CHANGE THIS (Please)😅
-                title: 'Project Three',
-                service: 'Web App',
+                title: 'Node Burgers',
+                service: '',
                 imageSrc: "https://images.unsplash.com/photo-1511500118080-275313ec90a1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
-                url: 'http://chetanverma.com/'
+                url: 'https://nodeburgers.herokuapp.com/burgers'
             },
             {
                 id: 4,//DO NOT CHANGE THIS (Please)😅
-                title: 'Project Four',
-                service: 'Branding',
+                title: 'Green Thumb',
+                service: 'Bootcamp Final Project',
                 imageSrc: "https://images.unsplash.com/photo-1558452919-08ae4aea8e29?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
-                url: 'http://chetanverma.com/'
+                url: ''
            }
 
                     /*
@@ -64,7 +64,7 @@ export default
         ],
         social: [
             // Add Or Remove The Link Accordingly
-            {   name:'Github',
+            {   name:'Click here to check out my Github',
                 url:'https://github.com/Brandenlanghals'},
             // {
             //     name: 'Behance',
